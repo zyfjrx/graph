2aedade457c6cbacb89566f3ec7cd8f0d36f99eb
zhangyf <zhangyifan0129@gmail.com>
2025-07-22 10:38:46 +0800

    fix bug

--- src/configs/config.py
+++ src/configs/config.py
@@ -5,3 +5,4 @@ DATA_DIR = ROOT_DIR / 'data'
 CHECKPOINT_DIR = ROOT_DIR / 'checkpoint'
 MODELS_DIR = ROOT_DIR / 'models'
 PRE_TRAINED_DIR = "/Users/zhangyf/llm/bert-base-chinese"
+EXTERNAL_LIB_DIR = ROOT_DIR / 'external_lib'
